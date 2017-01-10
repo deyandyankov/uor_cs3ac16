7d2f0b6eeedef275412e66a0eddcaae1dc4937fa
deyandyankov <deyan@dyankov.name>
2017-01-10 15:49:50 +0000

    Initial version - a hadoop skeleton

---
+++ src/main/java/uk/ac/reading/sse/App.java
@@ -0,0 +1,13 @@
+package uk.ac.reading.sse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
