6f6031d0030bf4f8884de9cbafafd60fc71ec5b7
deyandyankov <deyan@dyankov.name>
2017-02-01 09:06:48 +0000

    Move everything to root dir

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# nmr 
-Advanced Computing assignment
+# nmr
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+println("runtests: $(pwd())")
 testfiles = [f for f in readdir(".") if isfile(f) && startswith(f, "test_") && endswith(f, ".jl")]
 function run_test(testfile)
   info("Running test file $(testfile)")
